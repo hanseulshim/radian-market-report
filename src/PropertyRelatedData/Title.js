7e25886ccf2231eb5d007c4f16e32a7bad11ed47
Hanseul Shim <hanseulshim@gmail.com>
2019-12-13 00:55:21 -0500

    More updates

--- src/PropertyRelatedData/Title.js
+++ src/PropertyRelatedData/Title.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
 import moment from 'moment'
+import Profile from '../assets/profile.png'
+import { propertyData } from '../data/data.json'
 import { doc } from '../pdf'
+
+const { stats } = propertyData
 
 const Container = styled.div`
   margin-bottom: 25px;
   background: #f6f6f5;
   padding: 50px 50px 25px 50px;
+  display: flex;
+  justify-content: space-between;
 `
 
 const Date = styled.div`
-  text-align: right;
+  font-weight: bold;
+  margin-bottom: 10px;
 `
 const Report = styled.div`
   font-size: 150%;
@@ -18,6 +25,25 @@ const Report = styled.div`
 const Section = styled.div`
   font-weight: bold;
   font-size: 340%;
+`
+
+const ProfileContainer = styled.div`
+  position: relative;
+`
+
+const Image = styled.img`
+  width: 75px;
+  height: 75px;
+  position: absolute;
+  top: -15px;
+  left: -75px;
+`
+
+const Name = styled.div`
+  font-weight: bold;
+  font-size: 175%;
+  position: absolute;
+  top: ${props => (props.first ? '15px' : '40px')};
 `
 
 const Button = styled.button`
@@ -39,9 +65,18 @@ const Title = () => {
   return (
     <Container>
       {/* <Button onClick={() => doc.save('test.pdf')}>EXPORT PDF</Button> */}
-      <Date>{moment().format('MMMM DD, YYYY')}</Date>
-      <Report>Market Report</Report>
-      <Section>Property Related Data</Section>
+      <div>
+        <Report>Market Report</Report>
+        <Section>Property Related Data</Section>
+      </div>
+      <div>
+        <Date>{moment().format('MMMM DD, YYYY')}</Date>
+        <ProfileContainer>
+          <Image src={Profile} />
+          <Name first>{stats.agentFirst}</Name>
+          <Name>{stats.agentLast}</Name>
+        </ProfileContainer>
+      </div>
     </Container>
   )
 }
