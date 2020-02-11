9d32d600377f991acbccebae03679e2ca8cc3e8d
joneil@boostlabs.com <joneil@boostlabs.com>
2020-02-11 15:10:49 -0600

    pageFourInfo fetching data

--- src/DemographicData/PageFourInfo.js
+++ src/DemographicData/PageFourInfo.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import Text from '../common/Text'
-import { ageVsIncome } from '../data/data.json'
 
 const AgeVsIncome = styled.div`
   grid-area: info;
 `
 
 const PageFourInfo = () => {
+  const [ageVsIncome, setAgeVsIncome] = useState('')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('/data.json')
+      const data = await res.json()
+      setAgeVsIncome(data.ageVsIncome)
+    }
+    fetchData()
+  }, [])
   return (
     <AgeVsIncome>
       <Text h1> Age vs Income</Text>
