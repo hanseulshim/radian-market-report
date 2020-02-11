1a118ba9c1317f3796a656e37fe6fe63364aeeda
joneil@boostlabs.com <joneil@boostlabs.com>
2020-02-11 14:29:20 -0600

    family makeup info fetching

--- src/DemographicData/FamilyMakeupInfo.js
+++ src/DemographicData/FamilyMakeupInfo.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import Text from '../common/Text'
-import { familyMakeupInfo } from '../data/data.json'
+// import { familyMakeupInfo } from '../data/data.json'
 
 const Container = styled.div`
   grid-area: sectionInfo;
 `
 
 const FamilyMakeupInfo = () => {
+  const [familyMakeupInfo, setFamilyMakeupInfo] = useState('')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('/data.json')
+      const data = await res.json()
+      setFamilyMakeupInfo(data.familyMakeupInfo)
+    }
+    fetchData()
+  }, [])
+
   return (
     <Container>
       <Text chartTitle style={{ marginLeft: '0', marginBottom: '15px' }}>
